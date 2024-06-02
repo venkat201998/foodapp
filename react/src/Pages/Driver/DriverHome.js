858098bd60c3d8c5f417ee97d96836efba6680a7
Venkatanarasimha Reddy Burri <venkat@s-MacBook-Pro.lan>
2024-06-02 11:44:04 -0400

    home screen images

--- react/src/Pages/Driver/DriverHome.js
+++ react/src/Pages/Driver/DriverHome.js
@@ -1,8 +1,14 @@
 import React from 'react';
 
+import banner from '../../Assets/delivery-home.webp';
+
+import './index.css';
+
 const DriverHome = () => {
     return(
-        <></>
+        <div className='delivery-home'>
+            <img src={banner} alt='food-delivery-app' className='delivery-img' />
+        </div>
     )
 }
 
--- react/src/Pages/Partner/PartnerHome.js
+++ react/src/Pages/Partner/PartnerHome.js
@@ -1,6 +1,12 @@
+import banner from '../../Assets/partner-home.webp';
+
+import './index.css';
+
 const PartnerHome = () => {
     return(
-        <></>
+        <div  className='partner-home'>
+            <img src={banner} alt='food-partner-app' className='partner-img' />
+        </div>
     )
 }
 
